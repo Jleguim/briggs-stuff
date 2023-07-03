5209e718280d9cb438fdc1a6d1aa8c496f95bd00
Javier Leguizamo <jlegui745@gmail.com>
2023-07-03 06:46:59 -0500

    Backend update

---
+++ backend/discord/Controllers.js
@@ -0,0 +1,28 @@
+const User = require('./User.model')
+
+const api = require('./ApiCalls')
+
+module.exports.redirect = async function(req, res) {
+  res.sendStatus(200)
+}
+
+module.exports.exchange_code = async function(req, res) {
+  const accessResponse = await api.exchange_code(req.query.code, req.redirect_uri)
+  if (accessResponse.status == 200) return res.send(accessResponse.body)
+
+  res.status(401).send(accessResponse.body)
+}
+
+module.exports.refresh_token = async function(req, res) {
+  const refreshResponse = await api.refresh_token(req.query.refresh_token)
+  if (refreshResponse.status == 200) return res.send(refreshResponse.body)
+
+  res.send(refreshResponse.body)
+}
+
+module.exports.revoke_token = async function(req, res) {
+  const revokeResponse = await api.revoke_token(req.query.access_token)
+  if (revokeResponse.status == 200) return res.sendStatus(200)
+
+  res.send(revokeResponse.body)
+}
