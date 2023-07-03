1170a02e1919825f03709a7aac5973da59f4c2c3
Javier Leguizamo <jlegui745@gmail.com>
2023-07-03 15:51:43 -0500

    DiscordRoutes refactored

--- backend/discord/Controllers.js
+++ backend/discord/Controllers.js
@@ -1,26 +1,128 @@
-const api = require('./ApiCalls')
+const superagent = require('superagent')
+const url = require('url')
 
-module.exports.redirect = async function(req, res) {
+const User = require('../models/User.model')
+
+module.exports.redirect = (req, res) => res.sendStatus(200)
+
+module.exports.exchange = async function(req, res) {
+  var exchangeResponse = await superagent
+    .post('https://discord.com/api/v10/oauth2/token')
+    .set('Content-Type', 'application/x-www-form-urlencoded')
+    .send({
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      grant_type: 'authorization_code',
+      code: req.query.code,
+      redirect_uri: url.format({
+        protocol: req.protocol,
+        host: req.get('host'),
+        pathname: req.baseUrl + '/redirect'
+      })
+    })
+    .ok(res => res.status < 500)
+    .catch(err => console.log(err))
+
+  if (exchangeResponse.status != 200) {
+    return res.status(401).send(exchangeResponse.body)
+  }
+
+  const { access_token, refresh_token } = exchangeResponse.body
+  const userData = await getUserData(access_token)
+
+  if (!userData) return res.sendStatus(401)
+
+  const user = await User.findOrCreateByDiscord(userData.id, {
+    discordId: userData.id,
+    access_token: access_token,
+    refresh_token: refresh_token
+  })
+
+  await User.findByIdAndUpdate(user._id, {
+    discordId: userData.id,
+    access_token: access_token,
+    refresh_token: refresh_token
+  })
+
+  res.send({
+    discordId: user.discordId,
+    id: user._id,
+    access_token: access_token,
+    refresh_token: refresh_token
+  })
+}
+
+module.exports.refresh = async function(req, res) {
+  var refreshResponse = await superagent
+    .post('https://discord.com/api/v10/oauth2/token')
+    .set('Content-Type', 'application/x-www-form-urlencoded')
+    .send({
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      grant_type: 'refresh_token',
+      refresh_token: req.query.refresh_token
+    })
+    .ok(res => res.status < 500)
+    .catch(err => console.log(err))
+
+  if (refreshResponse.status != 200) {
+    return res.status(401).send(refreshResponse.body)
+  }
+
+  const { access_token, refresh_token } = refreshResponse.body
+  const userData = await getUserData(access_token)
+
+  if (!userData) return res.sendStatus(401)
+
+  const user = await User.findOrCreateByDiscord(userData.id, {
+    discordId: userData.id,
+    access_token: access_token,
+    refresh_token: refresh_token
+  })
+
+  await User.findByIdAndUpdate(user._id, {
+    discordId: userData.id,
+    access_token: access_token,
+    refresh_token: refresh_token
+  })
+
+  res.send({
+    discordId: user.discordId,
+    id: user._id,
+    access_token: access_token,
+    refresh_token: refresh_token
+  })
+}
+
+module.exports.revoke = async function(req, res) {
+  var revokeResponse = await superagent
+    .post('https://discord.com/api/v10/oauth2/token/revoke')
+    .set('Content-Type', 'application/x-www-form-urlencoded')
+    .send({
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      token: req.query.access_token
+    })
+    .ok(res => res.status < 500)
+    .catch(err => console.log(err))
+
+  if (revokeResponse.status != 200) {
+    return res.status(401).send(revokeResponse.body)
+  }
+
   res.sendStatus(200)
 }
 
-module.exports.exchange_code = async function(req, res) {
-  const accessResponse = await api.exchange_code(req.query.code, req.redirect_uri)
-  if (accessResponse.status == 200) return res.send(accessResponse.body)
+async function getUserData(access_token) {
+  var userResponse = await superagent
+    .get('https://discord.com/api/v10/users/@me')
+    .set('Authorization', 'Bearer ' + access_token)
+    .ok(res => res.status < 500)
+    .catch(err => console.log(err))
 
-  res.status(401).send(accessResponse.body)
-}
+  if (userResponse.status != 200) {
+    return undefined
+  }
 
-module.exports.refresh_token = async function(req, res) {
-  const refreshResponse = await api.refresh_token(req.query.refresh_token)
-  if (refreshResponse.status == 200) return res.send(refreshResponse.body)
-
-  res.send(refreshResponse.body)
-}
-
-module.exports.revoke_token = async function(req, res) {
-  const revokeResponse = await api.revoke_token(req.query.access_token)
-  if (revokeResponse.status == 200) return res.sendStatus(200)
-
-  res.send(revokeResponse.body)
+  return userResponse.body
 }
--- backend/discord/Router.js
+++ backend/discord/Router.js
@@ -1,14 +1,11 @@
 const express = require('express')
-const DiscordRoutes = express.Router()
+const AuthRoutes = express.Router()
 
 const controllers = require('./Controllers')
-const middleware = require('./Middleware')
 
-DiscordRoutes.use(middleware.getRedirectUri)
+AuthRoutes.get('/redirect', controllers.redirect)
+AuthRoutes.get('/exchange_code', controllers.exchange)
+AuthRoutes.get('/refresh_token', controllers.refresh)
+AuthRoutes.get('/revoke_token', controllers.revoke)
 
-DiscordRoutes.get('/redirect', controllers.redirect)
-DiscordRoutes.get('/exchange_code', controllers.exchange_code)
-DiscordRoutes.get('/refresh_token', controllers.refresh_token)
-DiscordRoutes.get('/revoke_token', controllers.revoke_token)
-
-module.exports = DiscordRoutes
+module.exports = AuthRoutes
