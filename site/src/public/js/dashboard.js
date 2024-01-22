cf31d423c923043c9b0f349fde9b32df6a4f3568
Javier Leguizamo <jlegui745@gmail.com>
2024-01-22 06:05:02 -0500

    Update dashboard.js

--- site/src/public/js/dashboard.js
+++ site/src/public/js/dashboard.js
@@ -1,14 +1,2 @@
-// async function main() {
-//   var jwt = document.cookies['jwt']
-//   if (!jwt) return window.open('/', '_self')
-
-//   var url = `${document.api}/api/discord/identify`
-//   var options = { headers: { jwt } }
-//   var response = await fetch(url, options)
-//   var body = await response.json()
-
-//   document.write(JSON.stringify(body, 0, 3))
-// }
-
-// main()
-document.writeln('hi')
+var jwt = document.cookies['jwt']
+if (!jwt) return window.open('/', '_self')
